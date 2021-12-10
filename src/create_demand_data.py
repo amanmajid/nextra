258c549e08ebf447c5b280d5b7e410548e5d99b2
Aman Majid <39566844+amanmajid@users.noreply.github.com>
2021-12-10 15:58:58 +0000

    Update header nomenclature in flow data

--- src/create_demand_data.py
+++ src/create_demand_data.py
@@ -43,7 +43,7 @@ def time_series_forecast(flows,fields,start,end,freq='H',exceptions=None):
             End date in MM/DD/YYYY format
 
         *freq* : str, default daily ('D')
-            Datetime frequency
+            datetime frequency
 
         *exceptions* : list
             Columns to neglect with respect to growth rate
@@ -55,13 +55,13 @@ def time_series_forecast(flows,fields,start,end,freq='H',exceptions=None):
 
     '''
 
-    new_flows = pd.DataFrame({'Date' : pd.date_range(start=start, end=end, freq=freq)})
-    new_flows['Day'] = new_flows.Date.dt.day
-    new_flows['Month'] = new_flows.Date.dt.month
-    new_flows['Year'] = new_flows.Date.dt.year
+    new_flows = pd.DataFrame({'date' : pd.date_range(start=start, end=end, freq=freq)})
+    new_flows['day'] = new_flows.date.dt.day
+    new_flows['month'] = new_flows.date.dt.month
+    new_flows['year'] = new_flows.date.dt.year
 
-    if 'Hour' in flows.columns.tolist():
-        new_flows['Hour'] = new_flows.Date.dt.hour
+    if 'hour' in flows.columns.tolist():
+        new_flows['hour'] = new_flows.date.dt.hour
 
     # loop over each column
     for f in fields:
@@ -70,9 +70,9 @@ def time_series_forecast(flows,fields,start,end,freq='H',exceptions=None):
         for i in tqdm(range(0,len(new_flows)),desc=f):
 
             try:
-                baseline_value = flows.loc[(flows.Hour==new_flows.at[i,'Hour']) & \
-                                           (flows.Day==new_flows.at[i,'Day'])   & \
-                                           (flows.Month==new_flows.at[i,'Month']),f].values[0]
+                baseline_value = flows.loc[(flows.hour==new_flows.at[i,'hour']) & \
+                                           (flows.day==new_flows.at[i,'day'])   & \
+                                           (flows.month==new_flows.at[i,'month']),f].values[0]
             except:
                 # Add mask value for leap year
                 baseline_value = numpy.nan
@@ -82,16 +82,16 @@ def time_series_forecast(flows,fields,start,end,freq='H',exceptions=None):
                 new_flows.at[i,f] = baseline_value
             else:
                 # get demand growth
-                if f.__contains__('Jordan'):
+                if f.__contains__('jordan'):
                     growth_rate = global_variables['jordan_demand_growth_rate']
-                elif f.__contains__('Israel'):
+                elif f.__contains__('israel'):
                     growth_rate = global_variables['israel_demand_growth_rate']
-                elif f.__contains__('West Bank'):
+                elif f.__contains__('west_bank'):
                     growth_rate = global_variables['palestine_demand_growth_rate']
-                elif f.__contains__('Gaza'):
+                elif f.__contains__('gaza'):
                     growth_rate = global_variables['palestine_demand_growth_rate']
                 # calculate
-                new_flows.at[i,f] = baseline_value * ( (1+growth_rate)**(new_flows.at[i,'Year']-flows.iloc[-1]['Year']) )
+                new_flows.at[i,f] = baseline_value * ( (1+growth_rate)**(new_flows.at[i,'year']-flows.iloc[-1]['year']) )
 
     return new_flows
 
@@ -102,26 +102,26 @@ flows = pd.read_csv('../data/_raw/flows/raw_flows.csv')
 start       = '1/1/2018'
 end         = '12/1/2031'
 # define fields to forecast and exceptions
-fields      = ['Israel energy demand',
-               'Jordan energy demand',
-               'West Bank energy demand',
-               'Gaza energy demand',
-               'Israel wind',
-               'Israel solar',
-               'Jordan wind',
-               'Jordan solar',
-               'West Bank wind',
-               'West Bank solar',
-               'Gaza wind',
-               'Gaza solar']
+fields      = ['israel_energy_demand',
+               'jordan_energy_demand',
+               'west_bank_energy_demand',
+               'gaza_energy_demand',
+               'israel_wind',
+               'israel_solar',
+               'jordan_wind',
+               'jordan_solar',
+               'west_bank_wind',
+               'west_bank_solar',
+               'gaza_wind',
+               'gaza_solar']
 
-exceptions  = ['Israel wind',
-               'Israel solar',
-               'Jordan wind',
-               'Jordan solar',
-               'West Bank wind',
-               'West Bank solar',
-               'Gaza wind',
+exceptions  = ['israel_wind',
+               'israel_solar',
+               'jordan_wind',
+               'jordan_solar',
+               'west_bank_wind',
+               'west_bank_solar',
+               'gaza_wind',
                'Gaza solar']
 
 # run
@@ -129,5 +129,5 @@ new_flows   = time_series_forecast(flows,fields,start,end,freq='H',exceptions=ex
 # save
 new_flows.to_csv('../data/nextra/nodal_flows/processed_flows.csv',index=False)
 # get an index of 2030
-new_flows= new_flows.loc[new_flows.Year==2030].reset_index(drop=True)
+new_flows= new_flows.loc[new_flows.year==2030].reset_index(drop=True)
 new_flows.to_csv('../data/nextra/nodal_flows/processed_flows_2030.csv',index=False)
